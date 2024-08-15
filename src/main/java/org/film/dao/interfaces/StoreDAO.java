3ad269317285321a1b7b6fc100d36bfdc9c5d374
Maksym Shkarbun <suprime28@yahoo.com>
2024-08-15 17:54:22 +0300

    Create some DAO interfaces

--- src/main/java/org/film/dao/interfaces/StoreDAO.java
+++ src/main/java/org/film/dao/interfaces/StoreDAO.java
@@ -3,6 +3,8 @@ package org.film.dao.interfaces;
 import org.film.entity.Store;
 
 public interface StoreDAO {
-    public void saveStore(Store store);
-    public void updateStore(Store store);
+    public void saveStoreById(Store store);
+    public void updateStoreById(Store store);
+    public void deleteStoreById(Store store);
+    public Store getStoreById(int id);
 }
