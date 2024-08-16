ea12d3118622752c7f5e70cce7b19632f9ee9211
Maksym Shkarbun <suprime28@yahoo.com>
2024-08-16 08:04:04 +0300

    A minor test of task

--- src/main/java/org/film/Main.java
+++ src/main/java/org/film/Main.java
@@ -1,7 +1,23 @@
 package org.film;
 
+import org.film.dao.iml.FilmDAOImpl;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, String> objectProperties = new HashMap<String, String>();
+        objectProperties.put("title", "myTitle");
+        objectProperties.put("description", "some description");
+        objectProperties.put("length", "30");
+        objectProperties.put("releaseYear", "2020");
+        objectProperties.put("language", "1");
+        objectProperties.put("originalLanguage", "1");
+        objectProperties.put("rentalRate", "9.5");
+        objectProperties.put("rentalDuration", "5");
+        objectProperties.put("replacementCost", "50.0");
+        objectProperties.put("specialFeatures", "Trailers");
+        FilmDAOImpl filmDAO = new FilmDAOImpl();
+        filmDAO.addNewFilm(objectProperties);
     }
 }
