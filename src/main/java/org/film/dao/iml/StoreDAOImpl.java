1e8c76570e1cc00f84f9f848e68b4b0de166a3d3
Maksym Shkarbun <suprime28@yahoo.com>
2024-08-16 08:01:10 +0300

    Release DAO implementation
    Store created and realised with the conditions of the task

---
+++ src/main/java/org/film/dao/iml/StoreDAOImpl.java
@@ -0,0 +1,93 @@
+package org.film.dao.iml;
+
+
+import org.film.dao.interfaces.StoreDAO;
+import org.film.entity.*;
+import org.film.hibernate.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class StoreDAOImpl implements StoreDAO {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    @Override
+    public void saveStoreById(Store store) {
+
+    }
+
+    public void customerRentalInventory(Customer customer, Store store, int filmId) {
+        Transaction tx = null;
+        try(Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            Query<Long> queryCount = session.createQuery("select count(*) from Inventory where film.id=:filmId and store.id=:storeId", Long.class);
+            queryCount.setParameter("filmId", filmId);
+            queryCount.setParameter("storeId", store.getId());
+            if(queryCount.uniqueResult() <= 0) throw new Exception("This movie is not available at this store.");
+
+
+            Query<Rental> queryRental = session.createQuery(
+                    "select r from Rental r where r.inventory.film.id=:filmId and r.inventory.store.id=:storeId and r.returnDate is null", Rental.class);
+            queryRental.setParameter("filmId", filmId);
+            queryRental.setParameter("storeId", store.getId());
+
+            Rental currentRental = queryRental.uniqueResult();
+
+            if (currentRental != null) throw new Exception("This film is unavailable right now.");
+
+            Query<Inventory> queryInventory = session.createQuery(
+                    "from Inventory where film.id=:filmId and store.id=:storeId", Inventory.class);
+            queryInventory.setParameter("filmId", filmId);
+            queryInventory.setParameter("storeId", store.getId());
+
+            Inventory availableInventory = queryInventory.setMaxResults(1).uniqueResult();
+            if (availableInventory == null) {
+                throw new Exception("Inventory is not available at this store.");
+            }
+
+            Rental rental = new Rental();
+            rental.setCustomer(customer);
+            rental.setInventory(availableInventory);
+            rental.setStaff(store.getManagerStaff());
+            rental.setReturnDate(Instant.now());
+            session.save(rental);
+
+            Payment payment = new Payment();
+            payment.setCustomer(customer);
+            payment.setStaff(store.getManagerStaff());
+            payment.setRental(rental);
+            payment.setAmount(BigDecimal.valueOf(9.99));
+            payment.setPaymentDate(Instant.now());
+            session.save(payment);
+
+            tx.commit();
+        } catch (Throwable e){
+            e.printStackTrace();
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                } catch (Throwable rollbackException) {
+                    rollbackException.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void updateStoreById(Store store) {
+
+    }
+
+    @Override
+    public void deleteStoreById(Store store) {
+
+    }
+
+    @Override
+    public Store getStoreById(int id) {
+        return null;
+    }
+}
